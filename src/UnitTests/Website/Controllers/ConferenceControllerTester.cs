609d2c5f9ade0246bc7b54e4dd9fae8d7b125259
jeffreypalermo@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <jeffreypalermo@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2007-12-29 05:06:28 +0000

    - added unit test for Schedule action on ConferenceController.
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%4036

---
+++ src/UnitTests/Website/Controllers/ConferenceControllerTester.cs
@@ -0,0 +1,51 @@
+using CodeCampServer.Model;
+using CodeCampServer.Model.Domain;
+using CodeCampServer.Model.Presentation;
+using CodeCampServer.Website.Controllers;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using Rhino.Mocks;
+
+namespace CodeCampServer.UnitTests.Website.Controllers
+{
+    [TestFixture]
+    public class ConferenceControllerTester
+    {
+        [Test]
+        public void ScheduleShouldGetConferenceByKeyAndSendScheduleToTheView()
+        {
+            MockRepository mocks = new MockRepository();
+            IConferenceRepository repository = mocks.CreateMock<IConferenceRepository>();
+            Conference conference = new Conference("austincodecamp2008", "Austin Code Camp");
+            SetupResult.For(repository.GetConferenceByKey("austincodecamp2008")).Return(conference);
+            mocks.ReplayAll();
+
+            TestingConferenceController controller = new TestingConferenceController(repository);
+            controller.Schedule("austincodecamp2008");
+
+            Assert.That(controller.ActualViewName, Is.EqualTo("showschedule"));
+            Schedule actualViewData = controller.ActualViewData as Schedule;
+            Assert.That(actualViewData, Is.Not.Null);
+            Assert.That(actualViewData.ConferenceName, Is.EqualTo("Austin Code Camp"));
+
+        }
+
+        private class TestingConferenceController : ConferenceController
+        {
+            public string ActualViewName;
+            public string ActualMasterName;
+            public object ActualViewData;
+
+            public TestingConferenceController(IConferenceRepository conferenceRepository) : base(conferenceRepository)
+            {
+            }
+
+            protected override void RenderView(string viewName, string masterName, object viewData)
+            {
+                ActualViewName = viewName;
+                ActualMasterName = masterName;
+                ActualViewData = viewData;
+            }
+        }
+    }
+}
