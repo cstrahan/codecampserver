d38403505347adda83e9696fa81e40e1379e4b6a
subdigital@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <subdigital@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2008-03-17 03:12:52 +0000

    deleting unused files ADDED:  speaker Equals and GetHashCode (value object) implementation
    All Model unit tests are passing....
    
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%40177

--- src/Model/Domain/Speaker.cs
+++ src/Model/Domain/Speaker.cs
@@ -1,16 +1,17 @@
+using System;
+
 namespace CodeCampServer.Model.Domain
 {
-    public class Speaker
+    public class Speaker : IEquatable<Speaker>
     {
-        private Person _person;
-        private string _speakerKey;
-        private string _bio;
-        private string _avatarUrl;
+        private readonly Person _person;
+        private readonly string _speakerKey;
+        private readonly string _bio;
+        private readonly string _avatarUrl;
 
         public Speaker()
         {
         }
-
 
         public Speaker(Person person, string speakerKey, string bio, string avatarUrl)
         {
@@ -43,6 +44,37 @@ namespace CodeCampServer.Model.Domain
         public virtual string GetName()
         {
             return Person.GetName();
+        }
+
+
+        public bool Equals(Speaker speaker)
+        {
+            if (speaker == null) return false;
+            if (!Equals(_person, speaker._person)) return false;
+            if (!Equals(_speakerKey, speaker._speakerKey)) return false;
+            if (!Equals(_bio, speaker._bio)) return false;
+            if (!Equals(_avatarUrl, speaker._avatarUrl)) return false;
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj)) return true;
+            return Equals(obj as Speaker);
+        }
+
+        public override int GetHashCode()
+        {
+            int result = _person.GetHashCode();
+            result = 29*result + _speakerKey.GetHashCode();
+            result = 29*result + _bio.GetHashCode();
+            result = 29*result + _avatarUrl.GetHashCode();
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return GetName();
         }
     }
 }
--- src/UnitTests/Model/Domain/SpeakerTester.cs
+++ src/UnitTests/Model/Domain/SpeakerTester.cs
@@ -1,4 +1,6 @@
+using CodeCampServer.Model.Domain;
 using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
 
 namespace CodeCampServer.UnitTests.Model.Domain
 {
@@ -8,7 +10,27 @@ namespace CodeCampServer.UnitTests.Model.Domain
         [Test]
         public void ShouldBeEqualIfAllPropertiesAreEqual()
         {
-            Assert.Fail("forget something?");
+            Person person = new Person("Barney", "Rubble", "brubble@gmail.com");
+            string bio = "test bio";
+            string avatarurl = "me.jpg";
+            string speakerKey = "key";
+            Speaker speaker = new Speaker(person, speakerKey, bio, avatarurl);
+            Speaker speaker2 = new Speaker(person, speakerKey, bio, avatarurl);
+
+            Assert.That(speaker, Is.EqualTo(speaker2));
+        }
+
+        [Test]
+        public void ShouldNOTBeEqualIfPropertiesAreNotEqual()
+        {
+            Person person = new Person("Barney", "Rubble", "brubble@gmail.com");
+            const string bio = "test bio";
+            const string avatarurl = "me.jpg";
+            const string speakerKey = "key";
+            Speaker speaker = new Speaker(person, speakerKey, bio, avatarurl);
+            Speaker speaker2 = new Speaker(person, speakerKey + "extra", bio, avatarurl);
+
+            Assert.That(speaker, Is.Not.EqualTo(speaker2));
         }
     }
 }
