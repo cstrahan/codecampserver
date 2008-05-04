3aa8fe0423f4d8e17ccc3932661232b899f8b9ff
subdigital@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <subdigital@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2008-05-04 19:25:06 +0000

    updated routes to use the new routes.MapRoute() helper method.  MUCH cleaner.
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%40215

--- src/Website/RouteConfigurator.cs
+++ src/Website/RouteConfigurator.cs
@@ -7,52 +7,19 @@ namespace CodeCampServer.Website
 	{
 		public virtual void RegisterRoutes()
 		{		    
-			var routes = RouteTable.Routes;        
+			var routes = RouteTable.Routes;
 
-			routes.Add(new Route("login/{action}",
-			                     new RouteValueDictionary(new {Controller = "login", Action = "index"}),
-			                     new MvcRouteHandler()));
-
-            routes.Add(new Route("conference/new",
-                                 new RouteValueDictionary(new {Controller="Conference", Action="new"}), 
-                                 new MvcRouteHandler()));            
-
-            routes.Add(new Route("admin/{action}",
-                                 new RouteValueDictionary(new{Controller="Admin", Action="index"}),
-                                 new MvcRouteHandler()));
-
-			routes.Add(new Route("{conferenceKey}/speaker/{speakerId}",
-								 new RouteValueDictionary(new { Controller = "speaker", Action = "view" }),
-								 new MvcRouteHandler()));
-
-
-		    routes.Add(new Route("{conferenceKey}/speakers/{action}",
-                                new RouteValueDictionary(new {action = "list", controller = "speaker"}),
-                                new MvcRouteHandler()));
-
-            routes.Add(new Route("{conferenceKey}/schedule/{action}",
-                                new RouteValueDictionary(new {action = "index", controller = "schedule"}),
-                                new MvcRouteHandler()));
-
-		    routes.Add(new Route("{conferenceKey}/sessions/{action}",
-		                        new RouteValueDictionary(new {Controller = "session", Action = "list"}),
-		                        new MvcRouteHandler()));
-
-		    routes.Add(new Route("{conferenceKey}/sponsors/{action}",
-		                        new RouteValueDictionary(new {Controller = "sponsor", Action = "list"}),
-		                        new MvcRouteHandler()));
-
-		    routes.Add(new Route("{conferenceKey}/sponsors/{action}/{sponsorName}",
-		                        new RouteValueDictionary(new {Controller = "sponsor", Action = "edit"}),
-		                        new MvcRouteHandler()));
-
-            routes.Add(new Route("{conferenceKey}/{action}",
-                                new RouteValueDictionary(new {action = "index", controller = "conference"}),
-                                new MvcRouteHandler()));
-
-		    routes.Add(new Route("Default.aspx",
-		                         new RouteValueDictionary(new { action = "current", controller = "conference" }),
-		                         new MvcRouteHandler()));				
+		    routes.MapRoute("login", "login/{action}", new {controller = "login", action = "index"});
+            routes.MapRoute("new-conference", "conference/new", new { controller = "conference", action = "new" });
+		    routes.MapRoute("admin", "admin/{action}", new {controller = "admin", action = "index"});
+            routes.MapRoute("speaker", "{conferenceKey}/speaker/{speakerId}", new { controller="speaker", action="view"});			
+            routes.MapRoute("speakers", "{conferenceKey}/speakers/{action}", new { controller="speaker", action="list" });
+		    routes.MapRoute("schedule", "{conferenceKey}/schedule/{action}", new { controller="schedule", action="index"});
+            routes.MapRoute("sessions", "{conferenceKey}/sessions/{action}", new { controller="session", action="list"});
+            routes.MapRoute("sponsors", "{conferenceKey}/sponsors/{action}", new { controller="sponsor", action="list"});
+            routes.MapRoute("sponsor-edit", "{conferenceKey}/sponsors/{action}/{sponsorName}", new { controller="sponsor", action="edit"});
+            routes.MapRoute("conference", "{conferenceKey}/{action}", new { controller = "conference", action = "details" });
+            routes.MapRoute("default_aspx", "Default.aspx", new { controller="conference", action="current" });            
 		}
 	}
 }
