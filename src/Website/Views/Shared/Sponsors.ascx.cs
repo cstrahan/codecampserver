76a4161e892a0f00b10f5e8d5fbc6da2a1889ae0
subdigital@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <subdigital@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2008-07-18 13:28:11 +0000

    ...
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%40271

---
+++ src/Website/Views/Shared/Sponsors.ascx.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+using CodeCampServer.Model.Domain;
+
+namespace CodeCampServer.Website.Views.Shared
+{
+    public class Sponsors : ViewUserControl<Sponsor[]>
+    {        
+    }
+}
