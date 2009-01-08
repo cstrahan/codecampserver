2c58ddf4189f55982f9d5ed11154fc230cce6548
eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2009-01-08 15:11:27 +0000

    More renames for the potential attendee.
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%40413

--- src/IntegrationTests/Infrastructure/DataAccess/ConferenceRepositoryTester.cs
+++ src/IntegrationTests/Infrastructure/DataAccess/ConferenceRepositoryTester.cs
@@ -1,7 +1,6 @@
 using System;
 using CodeCampServer.Core.Domain;
 using CodeCampServer.Core.Domain.Model;
-using CodeCampServer.Infrastructure.DataAccess.Impl;
 using NBehave.Spec.NUnit;
 using NHibernate;
 using NUnit.Framework;
@@ -10,80 +9,108 @@ using StructureMap;
 
 namespace CodeCampServer.IntegrationTests.Infrastructure.DataAccess
 {
-	[TestFixture]
-	public class ConferenceRepositoryTester : DataTestBase
-	{
-		[Test]
-		public void should_find_all_conferences()
-		{
-            var conference = new Conference { StartDate = new DateTime(2008, 12, 1), EndDate = new DateTime(2008, 12, 1) };
-            var conference1 = new Conference { StartDate = new DateTime(2008, 12, 1), EndDate = new DateTime(2008, 12, 1) };
+    [TestFixture]
+    public class ConferenceRepositoryTester : DataTestBase
+    {
+        private Conference CreateConference()
+        {
+            var conference = new Conference
+                                 {
+                                     Name = "sdf",
+                                     Description = "description",
+                                     StartDate = new DateTime(2008, 12, 2),
+                                     EndDate = new DateTime(2008, 12, 3),
+                                     LocationName = "St Edwards Professional Education Center",
+                                     Address = "12343 Research Blvd",
+                                     City = "Austin",
+                                     Region = "Tx",
+                                     PostalCode = "78234",
+                                     PhoneNumber = "512-555-1234"
+                                 };
+            conference.AddAttendee(new Attendee {EmailAddress = "werwer@asdfasd.com"});
+            return conference;
+        }
 
-			using (var session = GetSession())
-			{
-				session.SaveOrUpdate(conference);
-				session.SaveOrUpdate(conference1);
-				session.Flush();
-			}
+        [Test]
+        public void should_find_all_conferences()
+        {
+            var conference = new Conference {StartDate = new DateTime(2008, 12, 1), EndDate = new DateTime(2008, 12, 1)};
+            var conference1 = new Conference
+                                  {StartDate = new DateTime(2008, 12, 1), EndDate = new DateTime(2008, 12, 1)};
 
-			var repos = ObjectFactory.GetInstance<IConferenceRepository>();
+            using (ISession session = GetSession())
+            {
+                session.SaveOrUpdate(conference);
+                session.SaveOrUpdate(conference1);
+                session.Flush();
+            }
 
-			var persistedUsers = repos.GetAll();
-			CollectionAssert.AreEquivalent(new[] {conference, conference1}, persistedUsers);
-		}
+            var repos = ObjectFactory.GetInstance<IConferenceRepository>();
 
-		[Test]
-		public void Should_save_conference()
-		{
+            Conference[] conferences = repos.GetAll();
+            CollectionAssert.AreEquivalent(new[] {conference, conference1}, conferences);
+        }
+
+        [Test]
+        public void Should_get_by_id()
+        {
             Conference conference = CreateConference();
 
-			var repository = ObjectFactory.GetInstance<IConferenceRepository>();
-			repository.Save(conference);
+            using (ISession session = GetSession())
+            {
+                session.Save(conference);
+                session.Flush();
+            }
 
-			using (ISession session = GetSession())
-			{
+            var repository = ObjectFactory.GetInstance<IConferenceRepository>();
+            Conference conference1 = repository.GetById(conference.Id);
+            Assert.That(conference1, Is.EqualTo(conference));
+        }
+
+        [Test]
+        public void Should_save_conference()
+        {
+            Conference conference = CreateConference();
+
+            var repository = ObjectFactory.GetInstance<IConferenceRepository>();
+            repository.Save(conference);
+
+            using (ISession session = GetSession())
+            {
                 var rehydratedConference = session.Load<Conference>(conference.Id);
 
-				Assert.That(rehydratedConference.Id, Is.EqualTo(conference.Id));
-				Assert.That(rehydratedConference.Name, Is.EqualTo(conference.Name));
-				Assert.That(rehydratedConference.Description, Is.EqualTo(conference.Description));
+                Assert.That(rehydratedConference.Id, Is.EqualTo(conference.Id));
+                Assert.That(rehydratedConference.Name, Is.EqualTo(conference.Name));
+                Assert.That(rehydratedConference.Description, Is.EqualTo(conference.Description));
                 Assert.That(rehydratedConference.LocationName, Is.EqualTo(conference.LocationName));
                 Assert.That(rehydratedConference.StartDate, Is.EqualTo(conference.StartDate));
+                Assert.That(rehydratedConference.Attendees,Is.EqualTo(conference.Attendees));
             }
-		}
+        }
 
-	    private Conference CreateConference()
-	    {
-	        return new Conference()
-	                   {
-	                       Name = "sdf",
-	                       Description = "description",
-	                       StartDate = new DateTime(2008, 12, 2),
-	                       EndDate = new DateTime(2008, 12, 3),
-	                       LocationName = "St Edwards Professional Education Center",
-	                       Address = "12343 Research Blvd",
-	                       City = "Austin",
-	                       Region = "Tx",
-	                       PostalCode = "78234",
-	                       PhoneNumber = "512-555-1234"
+        [Test]
+        public void Should_remove_an_attendee_from_its_collection()
+        {
+            Conference conference = CreateConference();
+            conference.Attendees[0].Status = AttendanceStatus.NotAttending;
 
-	                   };
-	    }
+            var repository = ObjectFactory.GetInstance<IConferenceRepository>();
+            
+            repository.Save(conference);
 
-	    [Test]
-		public void Should_get_by_id()
-		{
-			var conference = CreateConference();
-
-			using (var session = GetSession())
-			{
-				session.Save(conference);
-				session.Flush();
-			}
-
-			var repository = ObjectFactory.GetInstance<IConferenceRepository>();
-			var conference1 = repository.GetById(conference.Id);
-			Assert.That(conference1, Is.EqualTo(conference));
-		}
-	}
+            Conference rehydratedConference;
+            using (ISession session = GetSession())
+            {
+                rehydratedConference = session.Load<Conference>(conference.Id);
+                rehydratedConference.Attendees[0].Status = AttendanceStatus.Confirmed;
+                session.SaveOrUpdate(rehydratedConference);
+                rehydratedConference = session.Load<Conference>(conference.Id);
+            }
+            rehydratedConference.Attendees[0].Status.ShouldEqual(AttendanceStatus.Confirmed);
+            
+            
+            
+            
+        }
+    }
 }
