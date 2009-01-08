e091c62b4eec19ff19901d939fb97b9ee9fb308e
eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2009-01-08 22:29:22 +0000

    Created TimeSlotUpdater.
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%40467

---
+++ src/Core/Services/Updaters/TimeSlotUpdater.cs
@@ -0,0 +1,59 @@
+using System;
+using CodeCampServer.Core.Domain;
+using CodeCampServer.Core.Domain.Model;
+
+namespace CodeCampServer.Core.Services.Updaters
+{
+	public class TimeSlotUpdater:ModelUpdater<TimeSlot,ITimeSlotMessage>,ITimeSlotUpdater
+	{
+		private readonly ITimeSlotRepository _repository;
+		private readonly IConferenceRepository _conferenceRepository;
+
+		public TimeSlotUpdater(ITimeSlotRepository repository, IConferenceRepository conferenceRepository)
+		{
+			_repository = repository;
+			_conferenceRepository = conferenceRepository;
+		}
+
+		protected override IRepository<TimeSlot> Repository
+		{
+			get { return _repository; }
+		}
+
+		protected override Guid GetIdFromMessage(ITimeSlotMessage message)
+		{
+			return message.Id;
+		}
+		
+		protected override UpdateResult<TimeSlot, ITimeSlotMessage> PreValidate(ITimeSlotMessage message)
+		{
+			var failureResult = new UpdateResult<TimeSlot, ITimeSlotMessage>(false);
+			bool messageHasFailedValidation = false;
+
+			DateTime result;			
+			if(!DateTime.TryParse(message.StartTime,out result) )
+			{
+				messageHasFailedValidation = true;
+				failureResult.WithMessage(m => m.StartTime, "Is not a valid Date and Time.");
+			}
+
+			if (!DateTime.TryParse(message.EndTime, out result))
+			{
+				messageHasFailedValidation = true;
+				failureResult.WithMessage(m => m.EndTime, "Is not a valid Date and Time.");
+			}
+
+			if (messageHasFailedValidation)
+				return failureResult;
+
+			return base.PreValidate(message);
+		}
+
+		protected override void UpdateModel(ITimeSlotMessage message, TimeSlot model)
+		{
+			model.StartTime = DateTime.Parse(message.StartTime);
+			model.EndTime = DateTime.Parse(message.EndTime);
+			model.Conference = _conferenceRepository.GetById(message.ConferenceId);
+		}
+	}
+}
---
+++ src/UnitTests/Core/Services/Updaters/TimeSlotUpdaterTester.cs
@@ -0,0 +1,103 @@
+using System;
+using CodeCampServer.Core.Domain;
+using CodeCampServer.Core.Domain.Model;
+using CodeCampServer.Core.Messages;
+using CodeCampServer.Core.Services.Updaters;
+using NBehave.Spec.NUnit;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace CodeCampServer.UnitTests.Core.Services.Updaters
+{
+	[TestFixture]
+	public class TimeSlotUpdaterTester : TestBase
+	{
+		[Test]
+		public void Should_save_a_new_timeslot_from_message()
+		{
+			var message = S<ITimeSlotMessage>();
+			message.StartTime = new DateTime(2009,5,30,9,0,0).ToString();
+			message.EndTime = new DateTime(2009, 5, 30, 10, 30, 0).ToString();
+			message.ConferenceId = Guid.NewGuid();
+			message.Id = Guid.Empty;
+			var conference = new Conference();
+			
+
+
+			var repository = M<ITimeSlotRepository>();
+			repository.Stub(x => x.GetById(message.Id)).Return(null);
+
+			var conferenceRepository = M<IConferenceRepository>();
+			conferenceRepository.Stub(x => x.GetById(message.ConferenceId)).Return(conference);
+
+			ITimeSlotUpdater updater = new TimeSlotUpdater(repository, conferenceRepository);
+
+			UpdateResult<TimeSlot, ITimeSlotMessage> result = updater.UpdateFromMessage(message);
+
+			result.Successful.ShouldBeTrue();
+			TimeSlot modelTimeSlot = result.Model;
+			modelTimeSlot.StartTime.ShouldEqual(new DateTime(2009, 5, 30, 9, 0, 0));
+			modelTimeSlot.EndTime.ShouldEqual(new DateTime(2009, 5, 30, 10,30, 0));
+			modelTimeSlot.Conference.ShouldEqual(conference);
+
+			repository.AssertWasCalled(r=>r.Save(modelTimeSlot));
+		}
+
+		[Test]
+		public void Should_update_a_timeslot_from_message()
+		{
+			var message = S<ITimeSlotMessage>();
+			message.StartTime = new DateTime(2009, 5, 30, 9, 0, 0).ToString();
+			message.EndTime = new DateTime(2009, 5, 30, 10, 30, 0).ToString();
+			message.ConferenceId = Guid.NewGuid();
+			message.Id = Guid.Empty;
+			var conference = new Conference();
+			var timeSlot = new TimeSlot();
+
+
+			var repository = M<ITimeSlotRepository>();
+			repository.Stub(x => x.GetById(message.Id)).Return(timeSlot);
+
+			var conferenceRepository = M<IConferenceRepository>();
+			conferenceRepository.Stub(x => x.GetById(message.ConferenceId)).Return(conference);
+
+			ITimeSlotUpdater updater = new TimeSlotUpdater(repository, conferenceRepository);
+
+			UpdateResult<TimeSlot, ITimeSlotMessage> result = updater.UpdateFromMessage(message);
+
+			result.Successful.ShouldBeTrue();
+			result.Model.ShouldEqual(timeSlot);
+			TimeSlot modelTimeSlot = result.Model;
+			modelTimeSlot.StartTime.ShouldEqual(new DateTime(2009, 5, 30, 9, 0, 0));
+			modelTimeSlot.EndTime.ShouldEqual(new DateTime(2009, 5, 30, 10, 30, 0));
+			modelTimeSlot.Conference.ShouldEqual(conference);
+
+			repository.AssertWasCalled(r => r.Save(modelTimeSlot));
+		}
+
+		[Test]
+		public void Should_validate_time_formats_in_the_message()
+		{
+			var message = S<ITimeSlotMessage>();
+			message.StartTime = "asdfd";
+			message.EndTime = "234234";
+			message.ConferenceId = Guid.NewGuid();
+			message.Id = Guid.Empty;
+			var conference = new Conference();
+
+			var repository = M<ITimeSlotRepository>();
+			repository.Stub(x => x.GetById(message.Id)).Return(null);
+
+			var conferenceRepository = M<IConferenceRepository>();
+			conferenceRepository.Stub(x => x.GetById(message.ConferenceId)).Return(conference);
+
+			ITimeSlotUpdater updater = new TimeSlotUpdater(repository, conferenceRepository);
+
+			UpdateResult<TimeSlot, ITimeSlotMessage> result = updater.UpdateFromMessage(message);
+
+			result.Successful.ShouldBeFalse();
+			result.GetMessages(m=>m.StartTime).Length.ShouldEqual(1);
+			result.GetMessages(m => m.EndTime).Length.ShouldEqual(1);
+		}
+	}
+}
