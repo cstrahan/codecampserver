469eaf9e29622db54a36213df95739327236727b
eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2009-02-04 20:14:38 +0000

    Updated unit tests for UrlGenerationTester Added updated Controller.tt template to generate unit test.... this is experimental.
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%40644

--- src/UnitTests/UI/Routes/UrlGenerationTester.cs
+++ src/UnitTests/UI/Routes/UrlGenerationTester.cs
@@ -1,29 +1,28 @@
+using System;
 using System.Web.Mvc;
 using System.Web.Routing;
 using CodeCampServer.UI;
 using MvcContrib.TestHelper;
 using NBehave.Spec.NUnit;
 using NUnit.Framework;
+using Rhino.Mocks;
 
 namespace CodeCampServer.UnitTests.UI.Routes
 {
-	[TestFixture,Ignore("There is a bug in the RC which has broken this functionality.")]
+	[TestFixture]//,Ignore("There is a bug in the RC which has broken this functionality.")]
 	public class UrlGenerationTester
 	{
-		[TestFixtureSetUp]
-		public void SetUp()
-		{
-			new RouteConfigurator().RegisterRoutes();
-		}
 
 		[Test]
 		public void Should_correctly_generate_session_url()
 		{
+			new RouteConfigurator().RegisterRoutes();
 			var builder = new TestControllerBuilder();
 			var context = new RequestContext(builder.HttpContext, new RouteData());
+			context.HttpContext.Response.Expect(x => x.ApplyAppPathModifier(null)).IgnoreArguments().Do(new Func<string, string>(s => s)).Repeat.Any();
 			var urlhelper = new UrlHelper(context);
-			string url = urlhelper.RouteUrl("session", new {sessionKey = "this-is-the-session"});
-			url.ShouldEqual("/sessions/this-is-the-session");
+			string url = urlhelper.RouteUrl("session", new { sessionKey = "this-is-the-session", conferenceKey = "austincodecamp" });
+			url.ShouldEqual("/austincodecamp/sessions/this-is-the-session");
 		}
 
 		[Test]
@@ -32,13 +31,12 @@ namespace CodeCampServer.UnitTests.UI.Routes
 			new RouteConfigurator().RegisterRoutes();
 			var builder = new TestControllerBuilder();
 			var context = new RequestContext(builder.HttpContext, builder.RouteData);
-
+			context.HttpContext.Response.Expect(x => x.ApplyAppPathModifier(null)).IgnoreArguments().Do(new Func<string, string>(s => s)).Repeat.Any();
 			var urlhelper =
-				new UrlHelper(
-					context);
-
-			string url = urlhelper.RouteUrl("speaker", new {conferenceKey = "austinCodeCamp", speakerKey = "ScottGuthrie"});
-			url.ShouldEqual("austinCodeCamp/speakers/ScottGuthrie");
+			  new UrlHelper(
+				context);
+			string url = urlhelper.RouteUrl("speaker", new { conferenceKey = "austinCodeCamp", speakerKey = "ScottGuthrie" });
+			url.ShouldEqual("/austinCodeCamp/speakers/ScottGuthrie");
 		}
 	}
 }
