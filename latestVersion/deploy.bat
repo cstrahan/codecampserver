dba6398640026f4bba20267828389f001c8f5073
eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2009-03-23 20:09:10 +0000

    added iis 7.0 commands to stop and start the website for deployment
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%40732

--- latestVersion/deploy.bat
+++ latestVersion/deploy.bat
@@ -1,4 +1,6 @@
 rd ..\codeToDeploy\ /s /q
 CodeCampServerPackage.exe -o..\CodeToDeploy\ -y
 cd ..\CodeToDeploy\
+%systemroot%\system32\inetsrv\appcmd stop site codecampserver
 dev.bat
+%systemroot%\system32\inetsrv\appcmd start site codecampserver
