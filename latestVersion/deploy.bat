9c5103110d1716dfea7468b8217d37eb933b8aef
eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2009-03-23 20:22:43 +0000

    added call command to invoke the dev.bat command.
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%40734

--- latestVersion/deploy.bat
+++ latestVersion/deploy.bat
@@ -2,5 +2,5 @@ rd ..\codeToDeploy\ /s /q
 CodeCampServerPackage.exe -o..\CodeToDeploy\ -y
 cd ..\CodeToDeploy\
 cmd /c %systemroot%\system32\inetsrv\appcmd stop site codecampserver
-dev.bat
+call dev.bat
 cmd /c %systemroot%\system32\inetsrv\appcmd start site codecampserver
