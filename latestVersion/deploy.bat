a22292e96c3e341641718740fae5884e67ad42c3
eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2009-03-23 20:19:04 +0000

    waits for the start and stop to finish
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%40733

--- latestVersion/deploy.bat
+++ latestVersion/deploy.bat
@@ -1,6 +1,6 @@
 rd ..\codeToDeploy\ /s /q
 CodeCampServerPackage.exe -o..\CodeToDeploy\ -y
 cd ..\CodeToDeploy\
-%systemroot%\system32\inetsrv\appcmd stop site codecampserver
+cmd /c %systemroot%\system32\inetsrv\appcmd stop site codecampserver
 dev.bat
-%systemroot%\system32\inetsrv\appcmd start site codecampserver
+cmd /c %systemroot%\system32\inetsrv\appcmd start site codecampserver
