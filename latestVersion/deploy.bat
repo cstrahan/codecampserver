eea9263ba4a53ec1df97ed0868b849df4d193165
eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2009-03-23 19:59:11 +0000

    removed zip files and replaced with the self extracting exe
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%40731

---
+++ latestVersion/deploy.bat
@@ -0,0 +1,4 @@
+rd ..\codeToDeploy\ /s /q
+CodeCampServerPackage.exe -o..\CodeToDeploy\ -y
+cd ..\CodeToDeploy\
+dev.bat
--- latestVersion/unzip.bat
+++
@@ -1,3 +0,0 @@
-del %1\*.* /S /Q
-rd %1
-7za  x CodeCampServerPackage.zip -o%1 -r
