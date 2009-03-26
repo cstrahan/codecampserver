8093dc8e3364a6e904f2937593e784ab2fc4bd31
eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2009-03-26 15:20:24 +0000

    made the SchemaExportTester explicit on the test level.  The Build server was not honoring the explict class level attribute
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%40756

--- src/IntegrationTests/TestHelper.cs
+++ src/IntegrationTests/TestHelper.cs
@@ -23,7 +23,7 @@ namespace CodeCampServer.IntegrationTests
 	[TestFixture(Description = "SchemaDrop"), Explicit]
 	public class SchemaImportTester
 	{
-		[Test, Category("SchemaDrop")]
+		[Test, Category("SchemaDrop"),Explicit]
 		public void DropSchema()
 		{
 			new SchemaExport(getSessionBuilder().GetConfiguration()).Drop(true, true);
