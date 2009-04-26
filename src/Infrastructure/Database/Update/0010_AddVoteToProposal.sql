8b7d12bfc10fa47a552f4f37a3693c10be04a0c9
jcteague@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <jcteague@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2009-04-26 21:47:14 +0000

    missing sql script
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%40831

---
+++ src/Infrastructure/Database/Update/0010_AddVoteToProposal.sql
@@ -0,0 +1,38 @@
+/*
+Script created by SQL Compare version 7.1.0 from Red Gate Software Ltd at 3/25/2009 3:52:14 PM
+Run this script on localhost\sqlexpress.CodeCampServerVersioned to make it the same as localhost\sqlexpress.CodeCampServer
+Please back up your database before running this script
+*/
+SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+IF EXISTS (SELECT * FROM tempdb..sysobjects WHERE id=OBJECT_ID('tempdb..#tmpErrors')) DROP TABLE #tmpErrors
+GO
+CREATE TABLE #tmpErrors (Error int)
+GO
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+GO
+BEGIN TRANSACTION
+GO
+PRINT N'Altering [dbo].[Speakers]'
+GO
+ALTER TABLE [dbo].[Proposals] ADD
+[Votes] [int] NULL
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+IF EXISTS (SELECT * FROM #tmpErrors) ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT>0 BEGIN
+PRINT 'The database update succeeded'
+COMMIT TRANSACTION
+END
+ELSE PRINT 'The database update failed'
+GO
+DROP TABLE #tmpErrors
+GO
