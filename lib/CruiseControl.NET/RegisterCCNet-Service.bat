3284d33b4f928cf4481a08f3244be2af1f114549
eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2009-10-16 13:42:20 +0000

    Added scripts to install ccnet as a service
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%40971

---
+++ lib/CruiseControl.NET/RegisterCCNet-Service.bat
@@ -0,0 +1,3 @@
+cd server
+%SystemRoot%\Microsoft.NET\Framework\v2.0.50727\installutil.exe /ServiceName="CCService 1.5" ccservice.exe
+pause
---
+++ lib/CruiseControl.NET/Uninstall CCNet-Service.bat
@@ -0,0 +1,3 @@
+cd server
+%SystemRoot%\Microsoft.NET\Framework\v2.0.50727\installutil.exe /u /ServiceName="CCService 1.5" ccservice.exe
+pause
