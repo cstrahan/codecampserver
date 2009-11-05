b42573ac3425867b418ea6833c79b1f3b867b3c5
eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2009-11-05 14:39:01 +0000

    updated deployment files and automapper configuration
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%401015

--- deployment/Dev.bat
+++ deployment/Dev.bat
@@ -1,14 +1,14 @@
-SET connectionString=Data Source=.\sqlexpress;Initial Catalog=CodeCampServer;Integrated Security=SSPI;
+SET connectionString=Data Source=.\sqlexpress;Initial Catalog=codecampserver_dev;Integrated Security=SSPI;
 SET driverClass=NHibernate.Driver.SqlClientDriver
 SET dialect=NHibernate.Dialect.MsSql2005Dialect
-SET websiteTargetDir=d:\domains\codecampserver\www
+SET websiteTargetDir=C:\inetpub\codecampserver_dev
 
 SET databaseServer=.\sqlexpress
-SET databaseName=CodeCampServer
+SET databaseName=codecampserver_dev
 SET databaseIntegrated=true
 SET databaseUsername=
 SET databasePassword=
 
 SET shouldReloadDatabase=false
 
-CommonDeploy.bat
+CommonDeploy.bat deploy loadDevData
--- src/Infrastructure/ObjectMapping/AutoMapperConfiguration.cs
+++ src/Infrastructure/ObjectMapping/AutoMapperConfiguration.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using AutoMapper;
+using CodeCampServer.Core;
 using CodeCampServer.DependencyResolution;
 using CodeCampServer.Infrastructure.ObjectMapping.ConfigurationProfiles;
 
@@ -11,13 +14,17 @@ namespace CodeCampServer.Infrastructure.ObjectMapping
 			Mapper.Initialize(x =>
 			                  	{
 			                  		x.ConstructServicesUsing(type => DependencyRegistrar.Resolve(type));
-			                  		x.AddProfile<AutoMapperProfile>();
-			                  		x.AddProfile<MeetingMapperProfile>();
-			                  		x.AddProfile<UserGroupMapperProfile>();
-			                  		x.AddProfile<MeetingMessageMapperProfile>();
-			                  		x.AddProfile<UserMapperProfile>();
-			                  		x.AddProfile<LoginMapperProfile>();
-			                  	});
+									GetProfiles().ForEach(type => x.AddProfile((Profile)Activator.CreateInstance(type)));
+								});
+		}
+
+		private static IEnumerable<Type> GetProfiles()
+		{
+			foreach (Type type in typeof(AutoMapperConfiguration).Assembly.GetTypes())
+			{
+				if (!type.IsAbstract && typeof(Profile).IsAssignableFrom(type))
+					yield return type;
+			}
 		}
 
 		void IRequiresConfigurationOnStartup.Configure()
