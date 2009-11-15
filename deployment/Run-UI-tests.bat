ae6d9456dbbfb640987b59cf0824fff6f5b5ec89
eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2009-11-15 22:32:09 +0000

    Added Ui tests using watin. Added scripts in the deployment to deploy an isolated application for ui testing.  The scripts are parameterize for creating multiple instances on a single machine without overlap of the web app or database with the developer instances.
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%401040

--- deployment/Run-UI-tests.bat
+++ deployment/Run-UI-tests.bat
@@ -1,5 +1,7 @@
 set port=85
 call CommonDeploy.bat .\sqlexpress codecampserver_local true
 start /min test\tools\cassini\ConsoleCassini-v35.exe  c:\inetpub\codecampserver_local %port%  /
-pause
+nant\nant.exe -buildfile:deployment.build pokeUIConfig -D:uitesturl=http://localhost:%port%
+test\tools\nunit\nunit-console.exe  tests\uitests.dll /noshadow /nologo /xml=uitests.results.xml
 taskkill /IM consolecassini-v35.exe
+pause
---
+++ src/UITests/HomePageTester.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using WatiN.Core;
+
+namespace UITests
+{
+	
+	[TestFixture]
+	public class HomePageTester
+	{
+		[STAThread]
+		[Test]
+		public void The_home_page_should_render()
+		{
+			var baseurl = System.Configuration.ConfigurationManager.AppSettings["url"];
+			using (var ie = new IE(baseurl))
+			{
+				ie.Link(Find.ByText("all events")).Click();
+				ie.Link(Find.ByText("about us")).Click();
+				ie.Link(Find.ByText("Log in")).Click();
+				ie.TextField("Username").TypeText("admin");
+				ie.TextField("Password").TypeText("password");
+				ie.Forms[0].Submit();
+				ie.Link(Find.ByText("My Profile")).Click();
+				ie.Link(Find.ByText("Log out")).Click();
+			}
+		}
+	}
+}
