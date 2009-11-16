2fdbf295222e5c57d6d7574aec5ec3c43767456b
eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2009-11-16 15:25:50 +0000

    updated the folder names of the test package.
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%401042

--- deployment/Run-UI-tests.bat
+++ deployment/Run-UI-tests.bat
@@ -1,6 +1,6 @@
 set port=85
 call CommonDeploy.bat .\sqlexpress codecampserver_local true
-start /min test\tools\cassini\ConsoleCassini-v35.exe  c:\inetpub\codecampserver_local %port%  /
+start /min tests\tools\cassini\ConsoleCassini-v35.exe  c:\inetpub\codecampserver_local %port%  /
 nant\nant.exe -buildfile:deployment.build pokeUIConfig -D:uitesturl=http://localhost:%port%
-test\tools\nunit\nunit-console.exe  tests\codecampserver.uitests.dll /noshadow /nologo /xml=uitests.results.xml
+tests\tools\nunit\nunit-console.exe  tests\codecampserver.uitests.dll /noshadow /nologo /xml=uitests.results.xml
 taskkill /IM consolecassini-v35.exe
