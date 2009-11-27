0cc3fde2e5875d9b9edb394e4c4d20e69a6505ee
jaeschke.tom3@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <jaeschke.tom3@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2009-11-27 17:23:32 +0000

    beginning integration for ThemeRoller
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%401052

---
+++ src/UI/Css.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeCampServer.UI
+{
+    public class Css
+    {
+        public const string UniversalColors = "ui-widget-content";
+        public const string UniversalFont = "ui-widget";
+    }
+}
---
+++ src/UI/Helpers/Extensions/ButtonExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeCampServer.UI.Helpers.Extensions
+{
+    public class ButtonExtensions
+    {
+    }
+}
