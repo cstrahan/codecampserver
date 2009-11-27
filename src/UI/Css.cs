7dadee23cf02797a60a837a69347cad2235c440c
jaeschke.tom3@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <jaeschke.tom3@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2009-11-27 23:19:07 +0000

    committing more ThemeRoller-related CSS skinning
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%401053

--- src/UI/Css.cs
+++ src/UI/Css.cs
@@ -2,6 +2,7 @@
 {
     public class Css
     {
+        public const string OuterControls = "ui-widget-header";
         public const string UniversalColors = "ui-widget-content";
         public const string UniversalFont = "ui-widget";
     }
