47d8f14a16f535d045a81ddb9a60b93ca2144860
jaeschke.tom3@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <jaeschke.tom3@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2009-11-29 04:56:05 +0000

    I'm adding another ThemeRoller change. This one makes links blue at the header and footer.
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%401054

--- src/UI/Css.cs
+++ src/UI/Css.cs
@@ -3,6 +3,7 @@
     public class Css
     {
         public const string OuterControls = "ui-widget-header";
+        public const string OuterLinks = "ui-state-highlight";
         public const string UniversalColors = "ui-widget-content";
         public const string UniversalFont = "ui-widget";
     }
