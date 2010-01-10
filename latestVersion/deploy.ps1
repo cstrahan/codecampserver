6ab110d7e5bfa1f419255a6591538f84aac7f910
erichexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <erichexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2010-01-10 22:35:45 +0000

    Converted the deploy.bat to a powershell script
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%401150

---
+++ latestVersion/deploy.ps1
@@ -0,0 +1,32 @@
+$application    = "CodeCampServer"
+$dbserver       = $args[0]
+$instance       = $args[1]
+$reloadDatabase = $args[2]
+$currentDir     = $args[3]
+
+#cd %4
+
+function SetDefaults( ){
+    if( !$dbserver ){$dbserver=".\sqlexpress"}
+    if( !$instance){$instance="local"}
+    if( !$reloadDatabase ){$reloadDatabase=false}
+}
+
+function DeployApp( $applicationName,$databaseServer,$instance,$reloadData ) {
+
+    $codedir="..\codeToDeploy_$instance\"
+    $appinstance="$applicationName_$instance"
+
+    if(test-path $codedir){ remove-item $codedir -Recurse }
+
+    & ".\$($applicationName)Package.exe" "-o$codedir" -y
+    
+    set-location $codedir
+
+    #cmd /c %systemroot%\system32\inetsrv\appcmd stop site $appinstance
+    #iisreset
+    & ".\CommonDeploy.bat" "$databaseServer" "$instance" "$reloadData"
+    #cmd /c %systemroot%\system32\inetsrv\appcmd start site %appinstance%
+}
+
+DeployApp -applicationName "$application" -databaseServer "$dbserver" -instance "$instance" -reloadData "$reloadDatabase"
