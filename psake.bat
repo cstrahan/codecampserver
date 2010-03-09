ab4972f355881a94fc27cca845e7cb755e1220d6
eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2010-03-09 02:49:24 +0000

    updated the psake.bat lets see if this fixes the TeamCity build
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%401171

--- psake.bat
+++ psake.bat
@@ -1,4 +1,3 @@
-@echo off 
 powershell -NoProfile -ExecutionPolicy unrestricted -Command "& {Import-Module '.\psake.psm1'; invoke-psake .\default.ps1; if ($Error -ne '') {write-host "ERROR: $error" -fore RED; exit $error.Count} }" 
 
 
