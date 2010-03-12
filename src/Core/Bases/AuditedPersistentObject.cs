09cc0ab1093a83dae004088035708a0842879d95
d.eric.anderson@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <d.eric.anderson@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2010-03-12 14:42:17 +0000

    Code Cleanup
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%401177

--- src/Core/Bases/AuditedPersistentObject.cs
+++ src/Core/Bases/AuditedPersistentObject.cs
@@ -3,63 +3,63 @@ using CodeCampServer.Core.Domain.Bases;
 
 namespace CodeCampServer.Core.Bases
 {
-    public abstract class AuditedPersistentObjectOfGuid : AuditedPersistentObject, IPersistentObjectOfGuid
-    {
-        public new virtual Guid Id
-        {
-            get { return (Guid) base.Id; }
-            set { base.Id = value; }
-        }
+	public abstract class AuditedPersistentObjectOfGuid : AuditedPersistentObject, IPersistentObjectOfGuid
+	{
+		public new virtual Guid Id
+		{
+			get { return (Guid) base.Id; }
+			set { base.Id = value; }
+		}
 
-        protected override object GetEmptyId()
-        {
-            return default(Guid);
-        }
+		protected override object GetEmptyId()
+		{
+			return default(Guid);
+		}
 
-    	public static Guid EmptyId = default(Guid);
-    }
+		public static Guid EmptyId = default(Guid);
+	}
 
-    public abstract class AuditedPersistentObjectOfInt32 : AuditedPersistentObject
-    {
-        public new virtual Int32 Id
-        {
-            get { return (int) base.Id; }
-            set { base.Id = value; }
-        }
+	public abstract class AuditedPersistentObjectOfInt32 : AuditedPersistentObject
+	{
+		public new virtual Int32 Id
+		{
+			get { return (int) base.Id; }
+			set { base.Id = value; }
+		}
 
-        protected override object GetEmptyId()
-        {
-            return default(Int32);
-        }
-    }
+		protected override object GetEmptyId()
+		{
+			return default(Int32);
+		}
+	}
 
-    public abstract class AuditedPersistentObject : PersistentObject, IAuditable
-    {
-        private ChangeAuditInfo _changeAuditInfo;
+	public abstract class AuditedPersistentObject : PersistentObject, IAuditable
+	{
+		private ChangeAuditInfo _changeAuditInfo;
 
-        protected AuditedPersistentObject()
-        {
-            _changeAuditInfo = new ChangeAuditInfo();
-        }
+		protected AuditedPersistentObject()
+		{
+			_changeAuditInfo = new ChangeAuditInfo();
+		}
 
-        public virtual ChangeAuditInfo ChangeAuditInfo
-        {
-            get
-            {
-                if (_changeAuditInfo == null)
-                    _changeAuditInfo = new ChangeAuditInfo();
+		public virtual ChangeAuditInfo ChangeAuditInfo
+		{
+			get
+			{
+				if (_changeAuditInfo == null)
+					_changeAuditInfo = new ChangeAuditInfo();
 
-                return _changeAuditInfo;
-            }
-            set
-            {
-                if (value == null)
-                    return;
+				return _changeAuditInfo;
+			}
+			set
+			{
+				if (value == null)
+					return;
 
-                _changeAuditInfo = value;
-            }
-        }
-    }
+				_changeAuditInfo = value;
+			}
+		}
+	}
 
 	public interface IPersistentObjectOfGuid
 	{
