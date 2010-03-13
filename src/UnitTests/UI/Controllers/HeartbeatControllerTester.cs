6e99e4b6693090c05733e6bb0ab3067e5d2b1e17
d.eric.anderson@25cf9c4d-fa41-0410-a78d-8fe958b11fb0 <d.eric.anderson@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2010-03-13 06:36:58 +0000

    Heartbeat complete and tested
    --HG--
    extra : convert_revision : svn%3A25cf9c4d-fa41-0410-a78d-8fe958b11fb0/trunk%401184

---
+++ src/UnitTests/UI/Controllers/HeartbeatControllerTester.cs
@@ -0,0 +1,68 @@
+using CodeCampServer.Core.Domain.Bases;
+using CodeCampServer.Core.Services;
+using CodeCampServer.UI.Controllers;
+using CodeCampServer.UI.Helpers.ActionResults;
+using CodeCampServer.UI.Models.Display;
+using CodeCampServer.UI.Models.Input;
+using MvcContrib.TestHelper;
+using NBehave.Spec.NUnit;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace CodeCampServer.UnitTests.UI.Controllers
+{
+	public class HeartbeatControllerTester : ControllerTester
+	{
+		[Test]
+		public void should_save_new_heartbeat()
+		{
+			var form = new HeartbeatInput {Message = "Howdy"};
+			var controller = new HeartbeatController(null, null);
+
+			var result = (CommandResult) controller.Edit(form);
+
+			result.Success.AssertActionRedirect().ToAction<HeartbeatController>(a => a.Index());
+		}
+
+		[Test]
+		public void should_list_on_index()
+		{
+			var all = new Heartbeat[3];
+
+			var repository = S<IHeartbeatRepository>();
+			repository.Stub(x => x.GetAll()).Return(all);
+
+			var controller = new HeartbeatController(repository, null);
+
+			var result = controller.Index();
+
+			result.ViewData.Model.ShouldBeTheSameAs(all);
+		}
+
+		[Test]
+		public void should_edit()
+		{
+			var controller = new HeartbeatController(null, null);
+
+			var result = controller.Edit();
+
+			var display = (HeartbeatInput) result.ViewData.Model;
+			display.Message.ShouldBeNull();
+		}
+
+		[Test]
+		public void should_check_the_heartbeat()
+		{
+			var timeout = 5;
+			var message = "something";
+			
+			var checker = S<IHeartbeatChecker>();
+			checker.Stub(x => x.CheckHeartbeat(timeout)).Return(message);
+
+			var controller = new HeartbeatController(null, checker);
+			var result = controller.Check(5);
+			var checkResult = (HeartbeatCheckDisplay)result.ViewData.Model;
+			checkResult.Message.ShouldEqual(message);
+		}
+	}
+}
