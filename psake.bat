d6662620fd17c791b2b0a265cdbf80993948ddb1
Eric Hexter <eric.hexter@gmail.com>
2010-04-01 22:42:27 -0500

    removed rem from psake build

--- psake.bat
+++ psake.bat
@@ -1,5 +1,4 @@
 powershell -NoProfile -ExecutionPolicy unrestricted -Command "& {Import-Module '.\psake.psm1'; invoke-psake .\default.ps1; if ($Error -ne '') {write-host "ERROR: $error" -fore RED; exit $error.Count} }" 
-rem -paramerters @{'buildNumber'='%1','databaseName'='%2'}
-pause
+
 
 
