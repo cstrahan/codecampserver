1db1375104563072ba6cb58813c25dba6772ce24
Eric Hexter <eric.hexter@gmail.com>
2010-04-30 14:09:18 -0500

    added a remote deployment bootstrapper batch file

---
+++ latestVersion/Remote.bat
@@ -0,0 +1,4 @@
+cd %1
+Shift
+powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& { import-module .\deploy.psm1 ; DeployRemote %~1 %~2 "%~3 %~4 %~5 %~6" if ($lastexitcode -ne 0) {write-host "ERROR: $lastexitcode" -fore RED; exit $lastexitcode}; stop-process $pid }" 
+exit
