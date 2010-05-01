a3692f1cea1d045f884177287ba467f338fb82c1
Eric Hexter <eric.hexter@gmail.com>
2010-04-30 23:02:46 -0500

    took a hard dependency on msdeploy and its default installation path.

--- latestVersion/pstrami.psm1
+++ latestVersion/pstrami.psm1
@@ -1,4 +1,24 @@
-
+function Send-Package {  
+    param([string]$server,[string]$destinationDirectory,[string]$cmd);
+    
+    #"args $cmd"
+    
+    $msdeployexe = resolve-path "C:\Program Files\IIS\Microsoft Web Deploy\msdeploy.exe"
+    
+    $sourceDirPath = resolve-path .
+    remove-item send-package.log
+    .$msdeployexe "-verb:sync" "-source:dirPath=$sourceDirPath" "-dest:dirPath=$destinationDirectory,computername=$server"        
+    
+    cmd.exe /c "$msdeployexe -verb:sync -dest:auto,computername=$server -source:runCommand=`"$destinationDirectory\pstrami-agent.bat $destinationDirectory $cmd`",waitInterval=2500,waitAttempts=20" | out-file "send-package.log"
+    
+    if(-not (select-string -path send-package.log -pattern "BUILD SUCCEEDED"))
+    {
+        get-content send-package.log | write-host
+        "Send-Package Failed"
+        exit '-1'
+    }
+    "Send-Package Succeeded"
+}
 
 function Receive-Package( $applicationName,$databaseServer,$instance,$reloadData) {
 
@@ -15,26 +35,7 @@ function Receive-Package( $applicationName,$databaseServer,$instance,$reloadData
 }
 
 
-function Send-Package
-{  
-param([string]$server,[string]$destinationDirectory,[string]$cmd);
-"args $cmd"
-    $msdeployexe = resolve-path "..\lib\msdeploy\msdeploy.exe"
-    
-    $sourceDirPath = resolve-path .
-    remove-item send-package.log
-    .$msdeployexe "-verb:sync" "-source:dirPath=$sourceDirPath" "-dest:dirPath=$destinationDirectory,computername=$server"        
-    
-    cmd.exe /c "$msdeployexe -verb:sync -dest:auto,computername=$server -source:runCommand=`"$destinationDirectory\pstrami-agent.bat $destinationDirectory $cmd`",waitInterval=2500,waitAttempts=20" | out-file "send-package.log"
-    
-    if(-not (select-string -path send-package.log -pattern "BUILD SUCCEEDED"))
-    {
-    get-content send-package.log | write-host
-        "Send-Package Failed"
-        exit '-1'
-    }
-    "Send-Package Succedded"
-}
+
 
 
 Export-ModuleMember Receive-Package, Send-Package
