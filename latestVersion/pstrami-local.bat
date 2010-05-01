15cac01e7f43364904c81106b335a2661dcc7981
Eric Hexter <eric.hexter@gmail.com>
2010-04-30 22:43:59 -0500

    adding local deployment capability

---
+++ latestVersion/pstrami-local.bat
@@ -0,0 +1,4 @@
+@echo off
+cd %1
+Shift
+powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& { import-module .\pstrami.psm1 ;  Receive-Package %~1 %~2 %~3 %~4 ; if ($lastexitcode -ne 0) {write-host "ERROR: $lastexitcode" -fore RED;exit $lastexitcode} }"
